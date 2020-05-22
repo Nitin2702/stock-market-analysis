5e06060b2e9c4160643c7fa8bcdf2b95240ed74f
GrayHat12 <grayhathacks10@gmail.com>
2020-05-22 13:56:08 +0530

    Updates

---
+++ precache-manifest.e732607d1fc73480f3e26e7874d2e1c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50ec3a4dfd33ed7ecdab04e08fea5aca",
+    "url": "/stock-market-analysis/index.html"
+  },
+  {
+    "revision": "c0708e23bc8b4d5d67e0",
+    "url": "/stock-market-analysis/static/css/main.74f170b8.chunk.css"
+  },
+  {
+    "revision": "a699c5bedb3a97217949",
+    "url": "/stock-market-analysis/static/js/2.4b29e0e4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/stock-market-analysis/static/js/2.4b29e0e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0708e23bc8b4d5d67e0",
+    "url": "/stock-market-analysis/static/js/main.07cf41f2.chunk.js"
+  },
+  {
+    "revision": "e0030cee08efa7f5db63",
+    "url": "/stock-market-analysis/static/js/runtime-main.1e117edb.js"
+  }
+]);
